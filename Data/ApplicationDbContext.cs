4173fd40e129fce4418b3425f90967ee758aa4be
Miguel <davidmiguelfloresmedrano@gmail.com>
2026-06-28 21:36:05 -0400

    Forzando nombres de tablas en minusculas

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,13 +11,22 @@ namespace DistribuidoraLosAndes.Data
 
         public DbSet<Rol> Roles { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
-
-        // LAS DOS LÍNEAS NUEVAS:
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Producto> Productos { get; set; }
-
-        //para los pedidos y el detalle del pedido 
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<PedidoDetalle> PedidoDetalles { get; set; }
+
+        // ESTE ES EL CÓDIGO NUEVO QUE OBLIGA AL SISTEMA A USAR MINÚSCULAS EXACTAS
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Rol>().ToTable("roles");
+            modelBuilder.Entity<Usuario>().ToTable("usuarios");
+            modelBuilder.Entity<Categoria>().ToTable("categorias");
+            modelBuilder.Entity<Producto>().ToTable("productos");
+            modelBuilder.Entity<Pedido>().ToTable("pedidos");
+            modelBuilder.Entity<PedidoDetalle>().ToTable("pedidodetalles");
+        }
     }
 }
